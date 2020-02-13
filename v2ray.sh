3422672504e756f42c03eb1e96df3631ef83b7ff
Hzbeta <ihzbeta@gmail.com>
2020-02-14 01:44:47 +0800

    install v2ray with custom uuid and port

---
+++ v2ray.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+Check_is_Root_in_CentOS() {
+    if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+        DISTRO='CentOS'
+    elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+        DISTRO='Debian'
+    elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+        DISTRO='Ubuntu'
+    elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+        DISTRO='Raspbian'
+    else
+        DISTRO='unknow'
+    fi
+    [[ $EUID -ne 0 ]] && echo "[Error] This script must be run as root" && exit 1
+    if [ "$DISTRO" != "CentOS" ]; then
+        echo "[Error] This script can only run in CentOS"
+        exit 1
+    fi
+    (rpm -q centos-release | grep '\-6\-') >/dev/null && (echo "[Error] This script can only run in CentOS 7+" && exit 1)
+}
+
+Check_is_Root_in_CentOS
+
+if [ -z "${1}" ] || [ -z "${2}" ]; then
+    echo -e "Usage: ${0} uuid port"
+    exit 0
+fi
+
+uuid=${1}
+port=${2}
+
+bash <(curl -s -L https://install.direct/go.sh)
+
+sed -i "s/\(\"port\"\s*:\)\s*[[:digit:]]\+/\1 ${port}/g" /etc/v2ray/config.json
+sed -i "s/\(\"id\"\s*:\)\s*\".*\"/\1 \"${uuid}\"/g" /etc/v2ray/config.json
+
+firewall-cmd --permanent --zone=public --add-port="${port}"/tcp
+firewall-cmd --reload
+
+systemctl start v2ray
