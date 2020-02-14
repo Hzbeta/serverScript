61f613d427ead04e037443265035ebef1ea99c6e
Hzbeta <ihzbeta@gmail.com>
2020-02-14 21:48:14 +0800

    add kcp/ws config

--- v2ray.sh
+++ v2ray.sh
@@ -22,10 +22,26 @@ Check_is_Root_in_CentOS() {
 
 Check_is_Root_in_CentOS
 
-if [ -z "${1}" ] || [ -z "${2}" ]; then
-    echo -e "Usage: ${0} uuid port"
+if [[ -z "${1}" || -z "${2}" ]]; then
+    echo -e "Usage: ${0} uuid port [kcp|ws]"
     exit 0
 fi
+
+if [ -n "${3}" ]; then
+    case "${3}" in
+    "kcp")
+        jqStr='.inbounds[0]+={"streamSettings":{"network":"mkcp","kcpSettings":{"header":{"type":"utp"}}}}'
+        ;;
+    "ws")
+        jqStr='.inbounds[0]+={"streamSettings":{"network":"ws"}}'
+        ;;
+    *)
+        echo -e "Usage: ${0} uuid port [kcp|ws]"
+        exit 0
+        ;;
+    esac
+fi
+
 
 uuid=${1}
 port=${2}
@@ -34,6 +50,12 @@ bash <(curl -s -L https://install.direct/go.sh)
 
 sed -i "s/\(\"port\"\s*:\)\s*[[:digit:]]\+/\1 ${port}/g" /etc/v2ray/config.json
 sed -i "s/\(\"id\"\s*:\)\s*\".*\"/\1 \"${uuid}\"/g" /etc/v2ray/config.json
+
+if [ -n "${jqStr}" ]; then
+    yum -y install jq
+    mv /etc/v2ray/config.json /etc/v2ray/config.bak
+    jq "${jqStr}" /etc/v2ray/config.bak >/etc/v2ray/config.json
+fi
 
 firewall-cmd --permanent --zone=public --add-port="${port}"/tcp
 firewall-cmd --reload
